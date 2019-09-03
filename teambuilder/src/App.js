76d444edbacf7dc164b606a0c364dc04ca5b32f0
Jenn Soderborg <jsoderborg7@gmail.com>
2019-09-03 14:34:54 -0600

    followercard set up

--- teambuilder/src/App.js
+++ teambuilder/src/App.js
@@ -8,12 +8,14 @@ export default class App extends Component {
   constructor(){
     super();
     this.state = {
-      userData: []
+      userData: [],
+      userFollowers: []
     }
   }
 
   componentDidMount(){
     this.getUser();
+    this.getFollowers();
   };
 
   getUser = () =>{
@@ -23,6 +25,19 @@ export default class App extends Component {
         console.log(res.data);
         const userData = res.data;
         this.setState({userData})
+      })
+      .catch(err =>{
+        console.log(err);
+      })
+  }
+
+  getFollowers = () =>{
+    axios
+      .get(`https://api.github.com/users/jsoderborg7/followers`)
+      .then(res =>{
+        console.log(res.data);
+        const userFollowers = res.data;
+        this.setState({userFollowers})
       })
       .catch(err =>{
         console.log(err);
