71915175001a1e8a48ef29436bc8619f2e21f0f0
Jenn Soderborg <jsoderborg7@gmail.com>
2019-09-03 13:55:19 -0600

    app file structured

--- teambuilder/src/App.js
+++ teambuilder/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      userData: [],
+      userFollowers: []
+    };
+  }
+
+  componentDidMount(){
+    this.getUsers();
+    this.getFollowers();
+  };
+
+  getUsers = () =>{
+    axios
+      .get('https://api.github.com/user/jsoderborg7')
+      .then(res => this.setState({userData: res.data}))
+      .catch(error => {console.log(error);});
+  };
+
+  getFollowers = () =>{
+    axios
+      .get('https://api.github.com/user/jsoderborg7/followers')
+      .then(res => this.setState({userFollowers: res.data}))
+      .catch(error => {console.log(error);});
+  };
+
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    )
+  }
 }
-
-export default App;
